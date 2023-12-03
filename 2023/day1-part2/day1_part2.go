a5c0865628c000975d17bdda93e755f97cda4b1b
Michal Przybylowicz <michal.przybylowicz@gmail.com>
2023-12-03 17:15:05 +0100

    refactor day1 into separate packages

--- 2023/day1-part2/day1_part2.go
+++ 2023/day1-part2/day1_part2.go
@@ -1,36 +1,11 @@
-package day1
+package day1_part2
 
 import (
 	"strconv"
 	"strings"
 )
 
-func sumOfAllTheCalibrationValues_part1(lines []string) int {
-	sum := 0
-	for _, line := range lines {
-		nums := ""
-		for _, ch := range line {
-			if ch >= '0' && ch <= '9' {
-				nums += string(ch)
-			}
-		}
-		if len(nums) == 1 {
-			nums = nums + nums
-		}
-		if len(nums) >= 3 {
-			nums = string(nums[0]) + string(nums[len(nums)-1])
-		}
-		val, err := strconv.Atoi(nums)
-		if err != nil {
-			panic(err)
-		}
-		sum += val
-	}
-
-	return sum
-}
-
-func sumOfAllTheCalibrationValues_part2(lines []string) int {
+func sumOfAllTheCalibrationValues(lines []string) int {
 	sum := 0
 	nums := []string{
 		"one",
