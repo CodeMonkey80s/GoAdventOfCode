ba89bbf7cb559056da148d88ab373c202178d3c5
Michal Przybylowicz <michal.przybylowicz@gmail.com>
2023-12-13 19:44:22 +0100

    add day4 part1 of 2022 refactor

--- 2022/day4_part1/day4_part1.go
+++ 2022/day4_part1/day4_part1.go
@@ -15,15 +15,9 @@ func pairsFullyContained(lines []string) int {
 	sum := 0
 	for _, line := range lines {
 		ranges := extractRanges(line)
-		if ranges[1].Lo >= ranges[0].Lo &&
-			ranges[1].Hi <= ranges[0].Hi {
+		ok := doRangesOverlap(ranges)
+		if ok {
 			sum++
-			continue
-		}
-		if ranges[0].Lo >= ranges[1].Lo &&
-			ranges[0].Hi <= ranges[1].Hi {
-			sum++
-			continue
 		}
 	}
 	return sum
@@ -41,4 +35,14 @@ func extractRanges(line string) []Range {
 		ranges = append(ranges, r)
 	}
 	return ranges
+}
+
+func doRangesOverlap(ranges []Range) bool {
+	switch {
+	case ranges[1].Lo >= ranges[0].Lo && ranges[1].Hi <= ranges[0].Hi:
+		return true
+	case ranges[0].Lo >= ranges[1].Lo && ranges[0].Hi <= ranges[1].Hi:
+		return true
+	}
+	return false
 }
--- 2022/day4_part1/day4_part1_test.go
+++ 2022/day4_part1/day4_part1_test.go
@@ -2,6 +2,7 @@ package day4_part1
 
 import (
 	"fmt"
+	"reflect"
 	"testing"
 
 	"GoAdventOfCode/util"
@@ -43,6 +44,79 @@ func Test_calculateSum(t *testing.T) {
 		label := fmt.Sprintf("Case: Input: %v Output: %v\n", len(tc.Input), tc.Output)
 		t.Run(label, func(t *testing.T) {
 			output := pairsFullyContained(tc.Input)
+			if output != tc.Output {
+				t.Errorf("Expected output to be %v but we got %v", tc.Output, output)
+			}
+		})
+	}
+}
+
+func Test_extractRanges(t *testing.T) {
+	testCases := []struct {
+		Input  string
+		Output []Range
+	}{
+		{
+			Input: "2-4,6-8",
+			Output: []Range{
+				{
+					Lo: 2,
+					Hi: 4,
+				},
+				{
+					Lo: 6,
+					Hi: 8,
+				},
+			},
+		},
+	}
+	for _, tc := range testCases {
+		label := fmt.Sprintf("Case: Input: %v Output: %v\n", len(tc.Input), tc.Output)
+		t.Run(label, func(t *testing.T) {
+			output := extractRanges(tc.Input)
+			if !reflect.DeepEqual(output, tc.Output) {
+				t.Errorf("Expected output to be %v but we got %v", tc.Output, output)
+			}
+		})
+	}
+}
+
+func Test_doRangesOverlap(t *testing.T) {
+	testCases := []struct {
+		Input  []Range
+		Output bool
+	}{
+		{
+			Input: []Range{
+				{
+					Lo: 2,
+					Hi: 8,
+				},
+				{
+					Lo: 3,
+					Hi: 7,
+				},
+			},
+			Output: true,
+		},
+		{
+			Input: []Range{
+				{
+					Lo: 6,
+					Hi: 6,
+				},
+				{
+					Lo: 4,
+					Hi: 6,
+				},
+			},
+			Output: true,
+		},
+	}
+	for _, tc := range testCases {
+		label := fmt.Sprintf("Case: Input: %v Output: %v\n", len(tc.Input), tc.Output)
+		t.Run(label, func(t *testing.T) {
+			output := doRangesOverlap(tc.Input)
 			if output != tc.Output {
 				t.Errorf("Expected output to be %v but we got %v", tc.Output, output)
 			}
