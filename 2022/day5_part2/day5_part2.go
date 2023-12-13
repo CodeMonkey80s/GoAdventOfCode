c1d69b63d123bc13ffb9fef36039c9c0ef7b7a37
Michal Przybylowicz <michal.przybylowicz@gmail.com>
2023-12-13 23:19:45 +0100

    add day5 part2 of 2022

---
+++ 2022/day5_part2/day5_part2.go
@@ -0,0 +1,120 @@
+package day5_part2
+
+import (
+	"fmt"
+	"strings"
+
+	"GoAdventOfCode/util"
+)
+
+type Stack struct {
+	Items []string
+}
+
+func (st *Stack) Initialize() {
+	st.Items = make([]string, 0)
+}
+
+func (st *Stack) Push(item string) {
+	st.Items = append(st.Items, item)
+}
+
+func (st *Stack) Pop(n int) []string {
+	a := len(st.Items) - n
+	b := len(st.Items)
+	items := st.Items[a:b]
+	st.Items = st.Items[:a]
+	return items
+}
+
+func (st *Stack) Last() string {
+	m := len(st.Items) - 1
+	return st.Items[m]
+}
+
+type Stacks struct {
+	Index map[int]*Stack
+}
+
+func (st *Stacks) Initialize(n int) {
+	st.Index = make(map[int]*Stack)
+	for i := 1; i <= n; i++ {
+		stack := Stack{}
+		stack.Initialize()
+		st.Index[i] = &stack
+	}
+}
+
+var CrateYard Stacks
+
+func finalCratesPositions(lines []string) string {
+	idx := findBreakingIndex(lines)
+	setupCrates(lines[:idx])
+	moveCrates(lines[idx+1:])
+	return getCratesPlacement()
+}
+
+func findBreakingIndex(lines []string) int {
+	for i, line := range lines {
+		if line == "" {
+			return i
+		}
+	}
+	return -1
+}
+
+func setupCrates(lines []string) {
+	stacksLine := lines[len(lines)-1]
+	stacksLine = strings.Replace(stacksLine, " ", "", -1)
+	stacksLine = strings.TrimRight(stacksLine, " ")
+	stacks := util.ConvertByteToInt(stacksLine[len(stacksLine)-1])
+	CrateYard.Initialize(stacks)
+	for i := len(lines) - 2; i >= 0; i-- {
+		line := lines[i][0:]
+		for j := 0; j < stacks; j++ {
+			crate := line[j*4 : (j*4)+3]
+			if crate == "   " {
+				continue
+			}
+			CrateYard.Index[j+1].Push(crate)
+		}
+	}
+}
+
+func moveCrates(lines []string) {
+	for _, line := range lines {
+		parts := strings.Fields(line)
+		n := util.ConvertStringToInt(parts[1])
+		a := util.ConvertStringToInt(parts[3])
+		b := util.ConvertStringToInt(parts[5])
+		crates := CrateYard.Index[a].Pop(n)
+		//for i := n - 1; i >= 0; i-- {
+		for i := 0; i < n; i++ {
+			crate := crates[i]
+			//fmt.Printf("Push: %v\n", crate)
+			CrateYard.Index[b].Push(crate)
+		}
+	}
+}
+
+func getCratesPlacement() string {
+	//printStacking()
+	crates := ""
+	for i := 1; i <= len(CrateYard.Index); i++ {
+		crates += string(CrateYard.Index[i].Last()[1])
+	}
+	return crates
+}
+
+func printStacking() {
+	for i := 1; i <= len(CrateYard.Index); i++ {
+		stack, ok := CrateYard.Index[i]
+		if ok {
+			fmt.Printf("%d -> ", i)
+			for _, item := range stack.Items {
+				fmt.Printf("%v", item)
+			}
+			fmt.Println()
+		}
+	}
+}
---
+++ 2022/day5_part2/day5_part2_test.go
@@ -0,0 +1,54 @@
+package day5_part2
+
+import (
+	"fmt"
+	"testing"
+
+	"GoAdventOfCode/util"
+)
+
+var testCases = []struct {
+	Input  []string
+	Output string
+}{
+	{
+		Input: []string{
+			"    [D]           ",
+			"[N] [C]           ",
+			"[Z] [M] [P]       ",
+			" 1   2   3        ",
+			"",
+			"move 1 from 2 to 1",
+			"move 3 from 1 to 3",
+			"move 2 from 2 to 1",
+			"move 1 from 1 to 2",
+		},
+		Output: "MCD",
+	},
+}
+
+func init() {
+	lines := util.LoadInputFile("../inputs/day5_input.txt")
+	testCase := []struct {
+		Input  []string
+		Output string
+	}{
+		{
+			Input:  lines,
+			Output: "VLCWHTDSZ",
+		},
+	}
+	testCases = append(testCases, testCase...)
+}
+
+func Test_calculateSum(t *testing.T) {
+	for _, tc := range testCases {
+		label := fmt.Sprintf("Case: Input: %v Output: %v\n", len(tc.Input), tc.Output)
+		t.Run(label, func(t *testing.T) {
+			output := finalCratesPositions(tc.Input)
+			if output != tc.Output {
+				t.Errorf("Expected output to be %v but we got %v", tc.Output, output)
+			}
+		})
+	}
+}
