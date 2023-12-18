1baf546c9205c1fabc361a496ae25c0aaa2d6065
Michal Przybylowicz <michal.przybylowicz@gmail.com>
2023-12-18 23:45:09 +0100

    refactor day1 part1 of 2023

--- 2023/day1_part1/day1_part1.go
+++ 2023/day1_part1/day1_part1.go
@@ -1,26 +1,27 @@
 package day1_part1
 
 import (
+	"unicode"
+
 	"GoAdventOfCode/util"
 )
 
 func sumOfAllTheCalibrationValues(lines []string) int {
 	sum := 0
 	for _, line := range lines {
-		nums := ""
-		for _, ch := range line {
-			if ch >= '0' && ch <= '9' {
-				nums += string(ch)
+		digits := make([]rune, 0)
+		for _, char := range line {
+			if unicode.IsDigit(char) {
+				digits = append(digits, char)
 			}
 		}
-		if len(nums) == 1 {
-			nums = nums + nums
+		switch {
+		case len(digits) == 1:
+			digits = append(digits, digits...)
+		case len(digits) >= 3:
+			digits = append([]rune{digits[0]}, digits[len(digits)-1])
 		}
-		if len(nums) >= 3 {
-			nums = string(nums[0]) + string(nums[len(nums)-1])
-		}
-		val := util.ConvertStringToInt(nums)
-		sum += val
+		sum += util.ConvertStringToInt(string(digits))
 	}
 
 	return sum
