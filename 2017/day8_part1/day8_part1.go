8d2e9edc910024da7f5a1e91d28eb14adb6529fd
Michal Przybylowicz <michal.przybylowicz@gmail.com>
2023-12-30 19:26:30 +0100

    add day8 part1 of 2017

---
+++ 2017/day8_part1/day8_part1.go
@@ -0,0 +1,111 @@
+package day8_part1
+
+import (
+	"fmt"
+	"slices"
+	"strings"
+
+	"GoAdventOfCode/util"
+)
+
+type Registers map[string]int
+
+func getAnswer(lines []string) int {
+
+	registers := make(Registers)
+
+	for _, line := range lines {
+
+		// 0    1         2    3  4    5     6
+		// [r1] [inc|dec] [v1] if [r2] [cmp] [v2]
+
+		parts := strings.Fields(line)
+		r1 := parts[0]
+		r2 := parts[4]
+		cmd := parts[1]
+		cmp := parts[5]
+		v1 := util.ConvertStringToInt(parts[2])
+		v2 := util.ConvertStringToInt(parts[6])
+
+		if _, ok := registers[r1]; !ok {
+			registers[r1] = 0
+		}
+		if _, ok := registers[r2]; !ok {
+			registers[r2] = 0
+		}
+
+		switch {
+
+		case cmd == "inc" && cmp == "==":
+			if registers[r2] == v2 {
+				registers[r1] += v1
+			}
+		case cmd == "inc" && cmp == "!=":
+			if registers[r2] != v2 {
+				registers[r1] += v1
+			}
+		case cmd == "inc" && cmp == ">":
+			if registers[r2] > v2 {
+				registers[r1] += v1
+			}
+		case cmd == "inc" && cmp == ">=":
+			if registers[r2] >= v2 {
+				registers[r1] += v1
+			}
+		case cmd == "inc" && cmp == "<":
+			if registers[r2] < v2 {
+				registers[r1] += v1
+			}
+		case cmd == "inc" && cmp == "<=":
+			if registers[r2] <= v2 {
+				registers[r1] += v1
+			}
+
+		case cmd == "dec" && cmp == "==":
+			if registers[r2] == v2 {
+				registers[r1] -= v1
+			}
+		case cmd == "dec" && cmp == "!=":
+			if registers[r2] != v2 {
+				registers[r1] -= v1
+			}
+		case cmd == "dec" && cmp == "<":
+			if registers[r2] < v2 {
+				registers[r1] -= v1
+			}
+		case cmd == "dec" && cmp == "<=":
+			if registers[r2] <= v2 {
+				registers[r1] -= v1
+			}
+		case cmd == "dec" && cmp == ">":
+			if registers[r2] > v2 {
+				registers[r1] -= v1
+			}
+		case cmd == "dec" && cmp == ">=":
+			if registers[r2] >= v2 {
+				registers[r1] -= v1
+			}
+		}
+	}
+
+	maxValue := 0
+	printRegisters(registers)
+	for _, v := range registers {
+		if v > maxValue {
+			maxValue = v
+		}
+	}
+
+	return maxValue
+}
+
+func printRegisters(registers Registers) {
+	keys := make([]string, 0, len(registers))
+	for k := range registers {
+		keys = append(keys, k)
+	}
+	slices.Sort(keys)
+	for _, v := range keys {
+		fmt.Printf("%v => \"%+v\"\n", v, registers[v])
+	}
+}
---
+++ 2017/day8_part1/day8_part1_test.go
@@ -0,0 +1,45 @@
+package day8_part1
+
+import (
+	"fmt"
+	"testing"
+
+	"GoAdventOfCode/util"
+)
+
+func Test_getAnswer(t *testing.T) {
+	var testCases = []struct {
+		Input  []string
+		Output int
+	}{
+		{
+			Input: []string{
+				"b inc 5 if a > 1",
+				"a inc 1 if b < 5",
+				"c dec -10 if a >= 1",
+				"c inc -20 if c == 10",
+			},
+			Output: 1,
+		},
+	}
+	lines := util.LoadInputFile("../inputs/day8_input.txt")
+	testCase := []struct {
+		Input  []string
+		Output int
+	}{
+		{
+			Input:  lines,
+			Output: 5143,
+		},
+	}
+	testCases = append(testCases, testCase...)
+	for _, tc := range testCases {
+		label := fmt.Sprintf("%v_%v\n", "Puzzle Input", tc.Output)
+		t.Run(label, func(t *testing.T) {
+			output := getAnswer(tc.Input)
+			if output != tc.Output {
+				t.Errorf("Expected output to be %v but we got %v", tc.Output, output)
+			}
+		})
+	}
+}
